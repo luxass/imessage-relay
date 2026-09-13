3b28e702543451a52d6d80d522273960544689ff
Lucas Nørgård <lucasnrgaard@gmail.com>
2026-09-13 09:21:42 +0200

    chore(main): release 0.1.0
    Set the initial release version to 0.1.0 in the release-please manifest and the server package version file.

--- Sources/relay-server/Application+build.swift
+++ Sources/relay-server/Application+build.swift
@@ -4,7 +4,7 @@ import Logging
 import RelayCore
 import ServiceLifecycle
 
-let packageVersion = "0.2.0" // x-release-please-version
+let packageVersion = "0.1.0" // x-release-please-version
 
 struct MessageStoreService: Service {
     let store: MessageStore
